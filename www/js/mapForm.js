80475829daa4715e5b7120103ff2e31eb8504795
Serhii Shulha <sv_shulga@ukr.net>
2017-08-31 11:17:50 +0300

    Map

--- www/js/mapForm.js
+++ www/js/mapForm.js
@@ -11,23 +11,23 @@ var MapForm = function() {
     this.init = function() {
         var scope = this;
         $('#map-wrapper').height($(window).height() - 100);
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var element = document.getElementById('map-canvas');
-            var lat = position.coords.latitude;
-            var lang = position.coords.longitude;
-            var myLatlng = new google.maps.LatLng(lat, lang);
-            var mapOptions = {
-                zoom: 4,
-                center: myLatlng
-            }
-            //Creates a new map inside the <div> element
-            scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-            //A marker identifies a location on a map.
-            var marker = new google.maps.Marker(
-                {
-                    position: myLatlng,
-                    map: scope.map
-                });
+        this.mapCanvas = document.getElementById("map-wrapper");
+        this.map = plugin.google.maps.Map.getMap(this.mapCanvas);
+        this.map.addEventListener(plugin.google.maps.event.MAP_READY, function() {
+            scope.onMapReady();
+        });
+    }
+
+    this.onMapReady = function() {
+        var scope = this;
+        this.map.getMyLocation(function(location) {
+            app.washForm.latlng = location.latLng;
+            scope.map.addMarker({
+                'position': location.latLng,
+                'title': msg
+            }, function(marker) {
+                marker.showInfoWindow();
+            });
         });
     }
 
