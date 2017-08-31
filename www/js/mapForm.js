a0e94f07a0efac7c791a8c504cc3fac655df17fb
Serhii Shulha <sv_shulga@ukr.net>
2017-08-31 12:02:22 +0300

    Map

--- www/js/mapForm.js
+++ www/js/mapForm.js
@@ -7,38 +7,44 @@ var MapForm = function() {
 
     this.map = null;
 
+    this.marker = null;
+
 
     this.init = function() {
         var scope = this;
         $('#map-wrapper').height($(window).height() - 100);
         this.mapCanvas = document.getElementById("map-wrapper");
         this.map = plugin.google.maps.Map.getMap(this.mapCanvas, {
-            
+
         });
         this.map.addEventListener(plugin.google.maps.event.MAP_READY, function() {
             scope.onMapReady();
         });
+        
     }
 
     this.onMapReady = function() {
+        var input = document.getElementById('map-input');
+        var searchBox = plugin.google.maps.places.SearchBox(input);
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchBox);
         this.setCurrentPosition();
     }
 
     this.setCurrentPosition = function() {
         var scope = this;
+        if(this.marker != null) {
+            this.marker.setMap(null);
+        }
+
         navigator.geolocation.getCurrentPosition(function(position) {
             var lat = position.coords.latitude;
             var lng = position.coords.longitude;
             scope.map.animateCamera({
                 target: {lat: lat, lng: lng},
-                zoom: 17,
-                tilt: 60,
-                bearing: 140,
-                duration: 5000
+                zoom: 17
             }, function() {
 
-                // Add a maker
-                scope.map.addMarker({
+               scope.marker = scope.map.addMarker({
                     position: {lat: lat, lng: lng},
                     animation: plugin.google.maps.Animation.BOUNCE
                 });
