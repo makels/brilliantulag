9d3708da834c2d49372a0c822e41948673b6a55f
Serhii Shulha <sv_shulga@ukr.net>
2017-09-01 13:26:52 +0300

    Map

--- www/js/mapForm.js
+++ www/js/mapForm.js
@@ -21,40 +21,13 @@ var MapForm = function() {
         this.map = plugin.google.maps.Map.getMap(this.mapCanvas);
         this.map.addEventListener(plugin.google.maps.event.MAP_READY, function() {
             scope.onMapReady();
-            scope.initAutocomplete();
-            $('body').on('touchstart','.pac-container', function(e){
-                e.stopImmediatePropagation();
-            })
         });
-
-    }
-
-    this.initAutocomplete = function() {
-        var scope = this;
-        try {
-        scope.autocomplete = new google.maps.places.Autocomplete(document.getElementById('map-autocomplete'), {types: ['geocode']});
-        scope.autocomplete.addListener('place_changed', function() {
-            var place = scope.autocomplete.getPlace();
-            app.washForm.latlng = {
-                lat: place.geometry.location.lat(),
-                lng: place.geometry.location.lat()
-            };
-            scope.map.animateCamera({
-                target: app.washForm.latlng,
-                zoom: 17
-            }, function() {
-                app.washForm.setAddress(app.washForm.latlng);
-                scope.marker = scope.map.addMarker({
-                    position: {lat: lat, lng: lng},
-                    animation: plugin.google.maps.Animation.BOUNCE
-                });
-            });
+        $('.pac-container').on('touchstart', function(e){
+            app.log("tap");
+            e.stopImmediatePropagation();
         });
-        } catch (e) {
-            app.log(e.message);
-        }
     }
-    
+ 
     this.onMapReady = function() {
         this.setCurrentPosition();
     }
